2b308358b686a81cf2b4d057cb7072a9565e011b
Tim <tulxoro@hotmail.com>
2025-10-14 18:50:58 -0700

    feat: added unused roles

---
+++ Core.cs
@@ -0,0 +1,74 @@
+﻿global using Il2Cpp;
+using Il2CppDissolveExample;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppInterop.Runtime.InteropTypes.Arrays;
+using Il2CppSystem.IO;
+using MelonLoader;
+using System;
+using UnityEngine;
+using static Il2Cpp.Interop;
+
+
+using static Il2CppSystem.Array;
+
+[assembly: MelonInfo(typeof(AlphaRoles.Core), "AlphaRoles", "0.1.0", "tulxoro", null)]
+[assembly: MelonGame("UmiArt", "Demon Bluff")]
+
+namespace AlphaRoles
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            // NOTHING FOR NOW
+        }
+
+        public override void OnLateInitializeMelon()
+        {
+            LoggerInstance.Msg("Attempting Late init...");
+
+            GameData gameData = Il2Cpp.ProjectContext.Instance.gameData;
+
+            // only two roles that are implemented and usable in the game...
+            CharacterData saintData = null;
+            CharacterData mutantData = null;
+
+            foreach (CharacterData character in gameData.allCharacterData)
+            {
+                
+                switch(character.characterId)
+                {
+                    case "Saint_61372493":
+                        saintData = character;
+                        break;
+                    case "Mutant_84675843":
+                        mutantData = character;
+                        break;
+            
+                }
+
+            }
+
+            AscensionsData advancedAscension = ProjectContext.Instance.gameData.advancedAscension;
+            foreach (CustomScriptData scriptData in advancedAscension.possibleScriptsData)
+            {
+                ScriptInfo script = scriptData.scriptInfo;
+                addRole(script.startingTownsfolks, saintData);
+                addRole(script.startingOutsiders, mutantData);
+                
+
+            }
+
+        }
+        public void addRole(Il2CppSystem.Collections.Generic.List<CharacterData> list, CharacterData data)
+        {
+            if (list.Contains(data))
+            {
+                return;
+            }
+            list.Add(data);
+        }
+
+    }
+}
---
+++ Marionette.cs
@@ -0,0 +1,68 @@
+﻿using Il2Cpp;
+using Il2CppFIMSpace.Basics;
+using Il2CppInterop.Runtime;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppInterop.Runtime.InteropTypes;
+using Il2CppSystem;
+using Il2CppSystem.Collections.Generic;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using UnityEngine;
+using static Il2CppSystem.Globalization.CultureInfo;
+using static MelonLoader.MelonLaunchOptions;
+
+
+public class Marionette : Il2Cpp.Minion
+{
+
+    public Marionette(IntPtr ptr) : base(ptr) { }
+
+
+    public override string Description
+    {
+        get => "[I sit next to a Demon]";
+    }
+
+    public override Il2Cpp.ActedInfo GetInfo(Il2Cpp.Character charRef)
+    {
+        return new Il2Cpp.ActedInfo("Sitting next to a demon...");
+    }
+
+    public override void Act(Il2Cpp.ETriggerPhase trigger, Il2Cpp.Character charRef)
+    {
+
+        if (trigger != Il2Cpp.ETriggerPhase.Start) return; 
+
+
+        SitNextToDemon(charRef);
+    }
+
+ 
+    public void SitNextToDemon(Il2Cpp.Character charRef)
+    {
+
+
+        List<Il2Cpp.Character> demons = 
+            new List<Il2Cpp.Character>(Il2Cpp.Gameplay.CurrentCharacters);
+        
+        demons = Il2Cpp.Characters.Instance.FilterCharacterType(demons, Il2Cpp.ECharacterType.Demon);
+
+        if (demons.Count <= 0) return;
+
+        Il2Cpp.Character pickedDemon = demons[UnityEngine.Random.Range(0, demons.Count)];
+        
+        System.Collections.Generic.List<Il2Cpp.Character> adjacentCharacters = 
+            Il2Cpp.Characters.Instance.GetAdjacentAliveCharacters(pickedDemon);
+        
+        if (adjacentCharacters.Count <= 0) return; 
+
+        Il2Cpp.Character pickedAdjacentCharacter = adjacentCharacters[UnityEngine.Random.Range(0, adjacentCharacters.Count)];
+        Il2Cpp.CharacterData pickedData = pickedAdjacentCharacter.dataRef;
+        
+        pickedAdjacentCharacter.InitWithNoReset(charRef.dataRef);
+        charRef.InitWithNoReset(pickedData);
+    }
+}
